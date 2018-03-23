776909cbd0a789f3aa68bc9e6aafd17204599523
Jim Lee <tin.le@groovetechnology.com>
2018-03-23 17:46:31 +0700

    Update entries component.

--- ff.words.site/ffwords/src/app/app.service.ts
+++ ff.words.site/ffwords/src/app/app.service.ts
@@ -1,14 +1,48 @@
 import { Http, Response, RequestOptionsArgs, RequestMethod, Headers } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import 'rxjs/add/observable/throw';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
 
-export abstract class AppService {
-    constructor(private _http: Http) { }
+export abstract class AppService extends BehaviorSubject<any> {
+    constructor(private _http: Http) {
+        super(null);
+    }
 
     public getData(sourceUrl: string, params?: any): Observable<Response> {
-        return this._http.get(sourceUrl, params);
+        return this._http.get(sourceUrl, params)
+            .map(response => parseResponse(response))
+            .catch(ex => {
+                console.log(ex);
+                return Observable.throw(ex);
+            });
     }
 
     public postData(url: string, data: any, params?: any): Observable<Response> {
-        return this._http.post(url, data, params);
+        return this._http.post(url, data, params)
+            .map(response => parseResponse(response))
+            .catch(ex => {
+                console.log(ex);
+                return Observable.throw(ex);
+            });
     }
+}
+
+export function parseResponse(response, isObject: boolean = false) {
+    let jsonResponse;
+    try {
+        jsonResponse = isObject ? response : <any>response.json();
+        for (let item in jsonResponse) {
+            if (jsonResponse[item] !== null) {
+                if (typeof (jsonResponse[item]) == "object") {
+                    parseResponse(jsonResponse[item], true);
+                }
+            }
+        }
+    } catch (ex) {
+        return null;
+    }
+
+    return jsonResponse;
 }
--- ff.words.site/ffwords/src/app/modules/entries/entries.component.ts
+++ ff.words.site/ffwords/src/app/modules/entries/entries.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { EntriesService } from './entries.service';
 import { AppUtils } from '../../shared/shared.utils';
+import { Observable } from 'rxjs/Observable';
+import { Promise } from 'q';
 
 @Component({
     selector: 'app-entries',
@@ -9,13 +11,20 @@ import { AppUtils } from '../../shared/shared.utils';
 })
 export class EntriesComponent implements OnInit {
 
+    listData;
+
     constructor(private service: EntriesService) {
-        this.service.getData(`${AppUtils.apiHost}api/Entry/GetEntries`)
-            .subscribe(data => {
-                if (data) {
-                    console.log(data);
-                }
-            });
+        //this.service.getData(`${AppUtils.apiHost}api/Entry/GetEntries`)
+        //    .subscribe(response => {
+        //        if (response) {
+        //            console.log(response);
+        //        }
+        //    });
+        this.listData = this.getList();
+    }
+
+    getList() {
+        return this.service.getData(`${AppUtils.apiHost}api/Entry/GetEntries`).toPromise<any>();
     }
 
     ngOnInit() {
