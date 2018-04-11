11e5a3e70b66c695bd6d4c3c29cb745b976805ca
Jim Lee <tin.le@groovetechnology.com>
2018-04-11 16:40:34 +0700

    Update entry form

--- ff.words.site/ffwords/src/app/modules/entries/entry/entry.component.ts
+++ ff.words.site/ffwords/src/app/modules/entries/entry/entry.component.ts
@@ -1,20 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewChecked } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Form } from '@angular/forms';
+import { Form, NgForm } from '@angular/forms';
 import { EntriesService } from '../entries.service';
 import { AppUtils } from '../../../shared/shared.utils';
 import { EntryModel } from '../../../shared/models/entry.model';
+import { NgControl } from '@angular/forms/src/directives/ng_control';
 
 @Component({
     selector: 'app-entry',
     templateUrl: './entry.component.html',
     styleUrls: ['./entry.component.css']
 })
-export class EntryComponent implements OnInit {
+export class EntryComponent implements AfterViewChecked, OnInit {
 
     public entryPromise: Promise<any> = null;
     public entryLoaded: boolean = false;
     public model: EntryModel;
+
+    mainForm: NgForm;
+    @ViewChild('mainForm') currentForm: NgForm;
 
     constructor(private route: ActivatedRoute,
         private service: EntriesService) {
@@ -36,5 +40,53 @@ export class EntryComponent implements OnInit {
     ngOnInit() {
     }
 
+    ngAfterViewChecked(): void {
+        this.formChanged();
+    }
 
+    formErrors = {};
+    validationRules = {};
+
+    formChanged() {
+        if (this.currentForm === this.mainForm) { return; }
+        this.mainForm = this.currentForm;
+
+        if (!AppUtils.isNullOrEmpty(this.mainForm)) {
+            this.mainForm.valueChanges.subscribe(
+                (data) => {
+                    this.onValueChanged(data);
+                });
+        }
+    }
+
+    validateAllFields(checkDirty: boolean) {
+        if (AppUtils.isNullOrEmpty(this.mainForm)) { return; }
+
+        const form = this.mainForm.form;
+        for (const field in this.validationRules) {
+            this.formErrors[field] = '';
+            const control = form.get(field);
+
+            if (!AppUtils.isNullOrEmpty(control)) {
+                if (!control.valid && (control.dirty || checkDirty)) {
+                    const message = this.validationRules[field];
+                    for (const key in control.errors) {
+                        this.formErrors[field] += message[key] + ' ';
+                    }
+                    control.markAsDirty();
+                }
+            }
+        }
+    }
+
+    onValueChanged(data?: any) {
+        this.validateAllFields(true);
+    }
+
+    onSubmit() {
+        this.service.postData(`${AppUtils.apiHost}Entry/UpdateEntry`, this.model)
+            .subscribe((data) => {
+                window.location.reload();
+            });
+    }
 }
--- ff.words.site/ffwords/src/app/shared/models/entry.model.ts
+++ ff.words.site/ffwords/src/app/shared/models/entry.model.ts
@@ -1,6 +1,7 @@
 export class EntryModel {
     public id: number;
     public title: string;
+    public content: string;
     public createdUser: string;
     public createdDate: Date;
 }
