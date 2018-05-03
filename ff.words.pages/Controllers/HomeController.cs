52408869073e0f94c38373d38d91ae2b6e859ec6
Jim Lee <tin.le@groovetechnology.com>
2018-05-03 17:19:07 +0700

    Remove contact page.

--- ff.words.pages/Controllers/HomeController.cs
+++ ff.words.pages/Controllers/HomeController.cs
@@ -39,13 +39,6 @@
             return View();
         }
 
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
